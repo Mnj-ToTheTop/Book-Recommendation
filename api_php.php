5d1a5cc941eca50b8bcf1ed3670091db0009a461
Manjara Raj <153396359+Mnj-ToTheTop@users.noreply.github.com>
2024-04-11 18:24:00 +0530

    Updateing api_php.php to work with sql

--- api_php.php
+++ api_php.php
@@ -52,29 +52,50 @@ echo $htmlOutput;
 
 if (isset($subjectData['works'])) {
   echo "<div id='books'>";
-  $count = 0; // Counter to track displayed works
-  foreach ($subjectData['works'] as $work) {
-    if ($count < 5) { // Only display up to 5 works
-      echo "<div class='book'>";
-
-      $authorName = isset($work['authors'][0]['name']) ? $work['authors'][0]['name'] : 'Unknown Author';
-      echo "<p class='books-title'>$work[title]</p>";
-      echo "<p class='books-author'>$authorName</p>";
-
-      if (isset($work['cover_id'])) {
-        $coverUrl = "http://covers.openlibrary.org/b/id/$work[cover_id].jpg";
-        echo "<br><img src='$coverUrl' alt='Book Cover Not Available'>";
+    
+    shuffle($subjectData['works']);
+  
+    $count = 0;
+    foreach ($subjectData['works'] as $work) {
+      if ($count < 5) {
+        $authorName = isset($work['authors'][0]['name']) ? $work['authors'][0]['name'] : 'Unknown Author';
+  
+        echo "<div class='book'>";
+        echo "<p class='books-title'>$work[title]</p>";
+        echo "<p class='books-author'>$authorName</p>";
+  
+        if (isset($work['cover_id'])) {
+          $coverUrl = "http://covers.openlibrary.org/b/id/$work[cover_id].jpg";
+          echo "<br><img src='$coverUrl' alt='Book Cover Not Available'>";
+        }
+  
+        // Add form to submit book title and author for wishlist
+        echo "<form action='add_to_wishlist.php' method='post'>";
+        echo "<input type='hidden' name='title' value='$work[title]'>";
+        echo "<input type='hidden' name='author' value='$authorName'>";
+        echo "<input type='hidden' name='cover' value='$coverUrl'>";
+        echo "<button type='submit' onclick='alert('Book Added')'>Add to Read List</button>";
+        echo "</form>";
+  
+        echo "</div>";
+        $count++;
       }
-      echo "</div>";
-      $count++;
     }
+    echo "</div>";
+  } else {
+    echo "No related works found for this subject.";
   }
-  echo "</div>";
-
-} else {
-  echo "No related works found for this subject.";
-}
 ?>
+
+</br>
+
+<h1> Want to search another genre? </h1></br>
+  <form id="forms" action="api_php.php" method="get" > <label for="subject">Enter another genre: </label>
+    <input type="text" id="subject" name="subject" oninput="validateInput()" required>
+    <div id='message'></div>
+    <br>
+    <button type="submit">Search</button>
+  </form></br>
 
 </body>
 </html>
